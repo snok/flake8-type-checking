88252e92642dd705dc1e75c2f527a3b54e6611e2
Sondre Lillebø Gundersen <sondrelg@otovo.com>
2021-02-28 20:22:44 +0000

    Correct disabled-by-default list

--- flake8_typing_only_imports/constants.py
+++ flake8_typing_only_imports/constants.py
@@ -6,4 +6,4 @@ if TYPE_CHECKING:
 TYO100 = "TYO100: Local import '{module}' only used for type hinting"  # noqa: FS003
 TYO101 = "TYO101: Remote import '{module}' only used for type hinting"  # noqa: FS003
 
-disabled_by_default: 'List[str]' = [TYO101]
+disabled_by_default: 'List[str]' = ['TYO101']
