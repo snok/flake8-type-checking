c3112b67d88335849a4861ca3a862bb145d56076
Sondre Lillebø Gundersen <sondrelg@otovo.com>
2021-04-02 16:19:41 +0200

    Fix failing tc004 test

---
+++ tests/test_tc004.py
@@ -0,0 +1,64 @@
+"""
+This file tests the TC004 error:
+
+    >> Move import out of type-checking block. Import is used for more than type hinting.
+
+"""
+import textwrap
+
+import pytest
+
+from flake8_type_checking.constants import TC004
+from tests import _get_error
+
+examples = [
+    # No error
+    ('', set()),
+    # Used in file
+    (
+        textwrap.dedent(
+            """
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from datetime import datetime
+
+    x = datetime
+    """
+        ),
+        {'5:0 ' + TC004.format(module='datetime')},
+    ),
+    # Used in function
+    (
+        textwrap.dedent(
+            """
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from datetime import date
+
+    def example():
+        return date()
+    """
+        ),
+        {'5:0 ' + TC004.format(module='date')},
+    ),
+    # Used for typing only
+    (
+        textwrap.dedent(
+            """
+    if TYPE_CHECKING:
+        from typing import Any
+
+    def example(*args: Any, **kwargs: Any):
+        return
+    """
+        ),
+        set(),
+    ),
+]
+
+
+@pytest.mark.parametrize('example, expected', examples)
+def test_TC004_errors(example, expected):
+    assert _get_error(example, error_code_filter='TC004') == expected
