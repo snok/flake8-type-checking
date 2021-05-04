f65372c992ecaaea415f15765e7b3d72a95b36a6
Sondre Lillebø Gundersen <sondrelg@otovo.com>
2021-05-04 13:10:51 +0200

    Correct type checking for nonetypes

--- flake8_type_checking/checker.py
+++ flake8_type_checking/checker.py
@@ -271,7 +271,7 @@ class ImportVisitor(ast.NodeTransformer):
     def _set_child_node_attribute(self, node: Any, attr: str, val: Any) -> Any:
         # Set the parent attribute on the current node children
         for key, value in node.__dict__.items():
-            if type(value) not in [int, str, list, bool, None]:
+            if type(value) not in [int, str, list, bool] and value is not None:
                 setattr(node.__dict__[key], attr, val)
         return node
 
