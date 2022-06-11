e55c963174a1583cc55455231cec981295b33d00
Sondre Lillebø Gundersen <sondrelg@live.no>
2022-06-11 19:17:23 +0200

    refactor: (c)attrs support
    Move attrs code to a separate mixin class, and refactor the flow a little

--- flake8_type_checking/constants.py
+++ flake8_type_checking/constants.py
@@ -3,5 +3,6 @@ import sys
 ATTRIBUTE_PROPERTY = '_flake8-type-checking_parent'
 
 ATTRS_DECORATORS = ['attrs.define', 'attr.define', 'attr.s']
+ATTRS_IMPORTS = {'attrs', 'attr'}
 
 py38 = sys.version_info.major == 3 and sys.version_info.minor == 8
